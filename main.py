436dd07e8a725659a58c6ad5612b2ed9b5453837
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2022-07-05 16:12:44 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import base64
+import fractions
+import decimal
+import math
+print('s')
+class a:
+    def __init__(s):
+        pass
+
+print(a())
+print(int(2.1))
+def encode(n):
+    return base64.b64encode(n)
+def b64(n):
+    return encode(bytes(n))
+print(b64(1))
+print(encode(b'a'))
+b=int()
+c=float()
+d=complex()
+e=fractions.Fraction(1,2)
+f=decimal.Decimal()
+g=bin(1)
+h=(1).to_bytes(1,'big')
+i=(1.0).hex()
+j=float.fromhex(i)
+k=math.isnan(1)
+l=math.isinf(1)
+print(b,c,d,e,f,g,h,i,j,k,l)
